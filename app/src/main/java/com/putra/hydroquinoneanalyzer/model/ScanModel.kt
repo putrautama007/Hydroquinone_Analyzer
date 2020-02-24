851e8ccfd31101c8296d5a44e920dd1a41d2946d
putrautama007 <putrautama30@gmail.com>
2020-02-24 12:26:33 +0700

    add activity

--- app/src/main/java/com/putra/hydroquinoneanalyzer/model/ScanModel.kt
+++ app/src/main/java/com/putra/hydroquinoneanalyzer/model/ScanModel.kt
@@ -5,49 +5,28 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "scanData")
-class ScanModel {
+class ScanModel(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    var scanId: Long = 0
+    var scanId: Long = 0,
 
     @ColumnInfo(name = "sampleName")
-    private var sampleName: String? = null
+    private var sampleName: String? = "",
 
     @ColumnInfo(name = "red")
-    private var red = 0
+    private var red: Int = 0,
 
     @ColumnInfo(name = "green")
-    private var green = 0
+    private var green: Int = 0,
 
     @ColumnInfo(name = "blue")
-    private var blue = 0
+    private var blue: Int = 0,
 
     @ColumnInfo(name = "concentration")
-    private var concentration = 0.0
+    private var concentration: Double = 0.0,
 
     @ColumnInfo(name = "concentrationPercentage")
-    private var concentrationPercentage = 0.0
+    private var concentrationPercentage: Double = 0.0,
 
     @ColumnInfo(name = "status")
-    private var status: String? = null
-
-    fun ScanModel(
-        scanId: Long,
-        sampleName: String?,
-        red: Int,
-        green: Int,
-        blue: Int,
-        concentration: Double,
-        concentrationPercentage: Double,
-        status: String?
-    ) {
-        this.scanId = scanId
-        this.sampleName = sampleName
-        this.red = red
-        this.green = green
-        this.blue = blue
-        this.concentration = concentration
-        this.concentrationPercentage = concentrationPercentage
-        this.status = status
-    }
-}
+    private var status: String? = ""
+)
---
+++ app/src/main/java/com/putra/hydroquinoneanalyzer/view/ScanResultActivity.kt
@@ -0,0 +1,13 @@
+package com.putra.hydroquinoneanalyzer.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.putra.hydroquinoneanalyzer.R
+
+class ScanResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_result)
+    }
+}
