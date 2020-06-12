16ca35a5edcb0bc998a6762327192380c65f9c91
putrautama007 <putrautama30@gmail.com>
2020-06-12 20:29:07 +0800

    update get rgb color

--- app/src/main/java/com/putra/hydroquinoneanalyzer/presenter/ScanResultPresenter.kt
+++ app/src/main/java/com/putra/hydroquinoneanalyzer/presenter/ScanResultPresenter.kt
@@ -14,30 +14,15 @@ class ScanResultPresenter(private val scanResultView: ScanResultView) {
     fun getAverageColorRGB(bitmap: Bitmap): IntArray {
         val width = bitmap.width / 2
         val height = bitmap.height / 2
-        var size = width * height
-        var pixelColor: Int
-        var r: Int
-        var g: Int
-        var b = 0
-        g = b
-        r = g
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                pixelColor = bitmap.getPixel(x, y)
-                if (pixelColor == 0) {
-                    size--
-                    continue
-                }
-                r += Color.red(pixelColor)
-                g += Color.green(pixelColor)
-                b += Color.blue(pixelColor)
-            }
-        }
-        r /= size
-        g /= size
-        b /= size
+
+        val pixelColor: Int
+        pixelColor = bitmap.getPixel(width, height)
+        val redValue = Color.red(pixelColor)
+        val blueValue = Color.blue(pixelColor)
+        val greenValue = Color.green(pixelColor)
+
         return intArrayOf(
-            r, g, b
+            redValue, greenValue, blueValue
         )
     }
 
